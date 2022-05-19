464e408113e312c9e8f3b8cbd475a1f085c91b66
Mahmoud.Elnemr <developer.elnemr@gmail.com>
2022-05-19 10:34:37 +0200

    setup request permissions

--- app/src/main/java/com/elnemr/runningtracker/presentation/ui/run/RunFragment.kt
+++ app/src/main/java/com/elnemr/runningtracker/presentation/ui/run/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.elnemr.runningtracker.presentation.ui.run
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
@@ -8,11 +10,15 @@ import androidx.navigation.fragment.findNavController
 import com.elnemr.runningtracker.R
 import com.elnemr.runningtracker.databinding.FragmentRunBinding
 import com.elnemr.runningtracker.presentation.base.view.BaseFragment
+import com.elnemr.runningtracker.presentation.util.Constants.REQUEST_CODE_LOCATION_PERMISSIONS
+import com.elnemr.runningtracker.presentation.util.PermissionUtils
 import com.elnemr.runningtracker.presentation.viewmodel.MainViewModel
 import com.elnemr.runningtracker.presentation.viewmodel.state.MainViewModelState
 import kotlinx.coroutines.flow.buffer
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class RunFragment() : BaseFragment(R.layout.fragment_run) {
+class RunFragment : BaseFragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: FragmentRunBinding
     private val viewModel by viewModels<MainViewModel>()
@@ -20,10 +26,29 @@ class RunFragment() : BaseFragment(R.layout.fragment_run) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRunBinding.bind(view)
-
+        requestPermissions()
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
+    }
+
+    private fun requestPermissions() {
+        if (PermissionUtils.hasLocationPermissions(requireContext())) return
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) EasyPermissions.requestPermissions(
+            this,
+            "You need to Accept location permissions to be able to use this app.",
+            REQUEST_CODE_LOCATION_PERMISSIONS,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+        else EasyPermissions.requestPermissions(
+            this,
+            "You need to Accept location permissions to be able to use this app.",
+            REQUEST_CODE_LOCATION_PERMISSIONS,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        )
     }
 
     override fun setUpViewModelStateObservers() {
@@ -38,5 +63,23 @@ class RunFragment() : BaseFragment(R.layout.fragment_run) {
         when (state) {
             else -> {}
         }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms))
+            AppSettingsDialog.Builder(this).build().show()
+        else requestPermissions()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
---
+++ app/src/main/java/com/elnemr/runningtracker/presentation/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.elnemr.runningtracker.presentation.util
+
+object Constants {
+    const val REQUEST_CODE_LOCATION_PERMISSIONS = 99
+}
