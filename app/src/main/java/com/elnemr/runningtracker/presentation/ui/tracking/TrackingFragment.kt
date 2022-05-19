91616a1444f38f5f4e5f643b9d800422d05c5893
Mahmoud.Elnemr <developer.elnemr@gmail.com>
2022-05-19 14:15:07 +0200

    setup mapview lifecycle

--- app/src/main/java/com/elnemr/runningtracker/presentation/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/com/elnemr/runningtracker/presentation/ui/tracking/TrackingFragment.kt
@@ -2,26 +2,29 @@ package com.elnemr.runningtracker.presentation.ui.tracking
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.elnemr.runningtracker.R
-import com.elnemr.runningtracker.databinding.FragmentRunBinding
-import com.elnemr.runningtracker.databinding.FragmentSetupBinding
 import com.elnemr.runningtracker.databinding.FragmentTrackingBinding
 import com.elnemr.runningtracker.presentation.base.view.BaseFragment
 import com.elnemr.runningtracker.presentation.viewmodel.MainViewModel
 import com.elnemr.runningtracker.presentation.viewmodel.state.MainViewModelState
+import com.google.android.gms.maps.GoogleMap
 import kotlinx.coroutines.flow.buffer
 
-class TrackingFragment() : BaseFragment(R.layout.fragment_tracking) {
+class TrackingFragment : BaseFragment(R.layout.fragment_tracking) {
 
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: FragmentTrackingBinding
+    private var map: GoogleMap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTrackingBinding.bind(view)
+
+        binding.mapView.getMapAsync {
+            map = it
+        }
     }
 
     override fun setUpViewModelStateObservers() {
@@ -36,4 +39,40 @@ class TrackingFragment() : BaseFragment(R.layout.fragment_tracking) {
         when (state) {
             else -> {}
         }
-    }}
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+}
