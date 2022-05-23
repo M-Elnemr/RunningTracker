274f6da0ed75d33ba55565cddf193dd8f805cb78
Mahmoud.Elnemr <developer.elnemr@gmail.com>
2022-05-23 14:31:56 +0200

    Revert "layout and navigation components"
    This reverts commit 6c4b0e27

--- app/src/main/java/com/elnemr/runningtracker/presentation/ui/setup/SetupFragment.kt
+++ app/src/main/java/com/elnemr/runningtracker/presentation/ui/setup/SetupFragment.kt
@@ -2,7 +2,6 @@ package com.elnemr.runningtracker.presentation.ui.setup
 
 import android.os.Bundle
 import android.view.View
-import androidx.navigation.fragment.findNavController
 import com.elnemr.runningtracker.R
 import com.elnemr.runningtracker.databinding.FragmentSetupBinding
 import com.elnemr.runningtracker.presentation.base.view.BaseFragment
@@ -16,7 +15,6 @@ class SetupFragment : BaseFragment(R.layout.fragment_setup) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSetupBinding.bind(view)
-
         binding.tvContinue.setOnClickListener {
             findNavController().navigate(R.id.action_setupFragment_to_runFragment)
         }
